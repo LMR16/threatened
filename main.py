f3cf64018ee21767cd8744bc8d84611eb47d3fc0
LMR16 <lukasristow@gmail.com>
2024-06-14 19:33:26 -0300

    ok

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import pandas as pd
 from pathlib import Path
-
+from sqlalchemy import create_engine
 
 path = Path("archive/especies.csv")
+engine = create_engine('postgresql://postgres:postgres@localhost:5432/threatened')
+
+def main():
+
+    csv = read_csv(path)
+
+    df = pd.DataFrame(csv)
+
+    filtered_data = df[(df["Grupão"] == "Fauna") & (df["Grupo"] == "Aves")]
+    
+    filtered_data.to_sql('threatended_fil', engine, if_exists="replace")
+
 
 def read_csv(path):
     csv = pd.read_csv(path, sep=';', encoding='latin-1')
     return csv
-
-csv = read_csv(path)
-
-print(csv)
